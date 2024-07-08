e322058956543f9d147c4d4e294e242386b026e2
Zakyboss <boochimo8@gmail.com>
2024-07-08 22:10:34 +0300

    function that achiev [200~Write a function that fills memory with a constant byte.
        Prototype: char *_memset(char *s, char b, unsigned int n);
        The _memset() function fills the first n bytes of the memory area pointed to by s with the constant byte b
        Returns a pointer to the memory area s
    
    ~

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @b: expected value
+ * @s: memory address to fill
+ * @n: number of bytes
+ *
+ * Return: a pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
