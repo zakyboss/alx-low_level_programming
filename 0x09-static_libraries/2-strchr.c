cb0c71410d50282dcd15ef653ca6ece3e0a68b84
Zakyboss <boochimo8@gmail.com>
2024-07-13 00:00:31 +0300

    Static libraries in C

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ *@s: string where pointer is returned
+ * @c: expected character
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+    	int r = 0;
+
+    	for (; s[r] >= '\0'; r++)
+    	{
+            	if (s[r] == c)
+            	return (&s[r]);
+   	}
+ return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+Today I am learning about static Libraries in C programming 
